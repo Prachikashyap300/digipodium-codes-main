2585abff2ae5aea370db95c72ada0e6f3e5f42ce
jyotic1498 <114425971+jyotic1498@users.noreply.github.com>
2024-04-06 09:04:29 +0530

    new

--- digipodium-codes-main/Frontend/src/App.jsx
+++ digipodium-codes-main/Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import './App.css'
-import Signup from './components/Signup'
+import Signup from './components/signup'
 import Login from './components/Login'
 import EventHandling from './components/EventHandling'
 import {SnackbarProvider} from 'notistack'
@@ -11,16 +11,16 @@ const App = () => {
   return (
     <div>
       <SnackbarProvider>
-      <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/Home' element={<Home />} />
-        <Route path='/Signup' element={<Signup />} />
-        <Route path='/Login' element={<Login />} />
-        <Route path='/EventHandling' element={<EventHandling />} />
-      </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/Home' element={<Home />} />
+            <Route path='/Signup' element={<Signup />} />
+            <Route path='/Login' element={<Login />} />
+            <Route path='/EventHandling' element={<EventHandling />} />
+          </Routes>
+          </BrowserRouter>
       </SnackbarProvider>
     </div>
   )
